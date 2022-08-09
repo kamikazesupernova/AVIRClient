2b45e42ae938ce29c82c4a21ba1b8b3ccc02441c
kamikazesupernova <sass4shure@gmail.com>
2022-08-09 21:20:03 +0100

    finished process

--- AVIRclient.c
+++ AVIRclient.c
@@ -7,6 +7,7 @@
 #include <curl\curl.h>
 #include <json-c\json.h>
 
+
 /* holder for curl fetch */
 struct curl_fetch_st {
     char *payload;
@@ -14,7 +15,71 @@ struct curl_fetch_st {
 };
 
 static char *conf_file_name = "testReport.json";
+static char *conf_file_profiles = "testProfiles.json";
+static char *url_IncidentReport ="https://localhost:7106/api/IncidentReport";
+static char *url_ThreatReport ="https://localhost:7106/api/ThreatProfile";
 json_object *json_root = NULL;
+json_object *jobj = NULL;
+
+void clearFile()
+{
+  fclose(fopen(conf_file_name, "w"));
+}
+
+static boolean addProfile(){
+      // create file if it doesn't exist
+    FILE* fp;
+
+    fp = fopen(conf_file_profiles, "r");
+    if (!fp)
+    {
+       fp = fopen(conf_file_profiles, "w");
+       if (!fp)
+           return 0;
+       fputs("[]", fp);
+       fclose(fp);
+    }
+
+     // add the document to the file
+    fp = fopen(conf_file_profiles, "rb+"); 
+    if(fp)
+    {
+        // check if first is [
+       fseek(fp, 0, SEEK_SET);
+       if (getc(fp) != '[')
+       {
+           fclose(fp);
+          return FALSE;
+       }
+       // is array empty?
+       boolean is_empty = FALSE;
+       if (getc(fp) == ']')
+           is_empty = TRUE;
+
+      // check if last is ]
+       fseek(fp, -1, SEEK_END);
+       if (getc(fp) != ']')
+       {
+           fclose(fp);
+           return FALSE;
+       }
+
+      // replace ] by ,
+        fseek(fp, -1, SEEK_END);
+        if (!is_empty)
+            fputc(',', fp);
+
+       // append the document
+       fputs(json_object_to_json_string(jobj), fp);
+
+       // close the array
+       fputc(']', fp);
+       fclose(fp);
+
+       return TRUE;
+    }
+    return FALSE;
+}
 
 /* callback for curl fetch */
 size_t curl_callback (void *contents, size_t size, size_t nmemb, void *userp) {
@@ -93,8 +158,7 @@ CURLcode curl_fetch_url(CURL *ch, const char *url, struct curl_fetch_st *fetch) 
     return rcode;
 }
 
-
-int postRequest(){
+int postRequest(char *url, char *endpoint){
     CURL *ch;                                               /* curl handle */
     CURLcode rcode;                                         /* curl result code */
 
@@ -104,9 +168,10 @@ int postRequest(){
     struct curl_fetch_st curl_fetch;                        /* curl fetch struct */
     struct curl_fetch_st *cf = &curl_fetch;                 /* pointer to fetch struct */
     struct curl_slist *headers = NULL;                      /* http headers to send with request */
+    jobj = NULL;
 
     /* url to test site */
-    char *url = "https://localhost:7106/api/IncidentReport";
+    /* char *url = "https://localhost:7106/api/IncidentReport"; */
 
     /* init curl handle */
     if ((ch = curl_easy_init()) == NULL) {
@@ -124,6 +189,7 @@ int postRequest(){
     curl_easy_setopt(ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_easy_setopt(ch, CURLOPT_HTTPHEADER, headers);
     curl_easy_setopt(ch, CURLOPT_POSTFIELDS, json_object_to_json_string(json_root));
+
 
     /* fetch page and capture return code */
     rcode = curl_fetch_url(ch, url, cf);
@@ -153,6 +219,7 @@ int postRequest(){
         /* parse return */
         json = json_tokener_parse_verbose(cf->payload, &jerr);
         /* free payload */
+
         free(cf->payload);
     } else {
         /* error */
@@ -173,8 +240,11 @@ int postRequest(){
         return 4;
     }
 
+    //set return object
+     jobj = json;
+
     /* debugging */
-    printf("Parsed JSON: %s\n", json_object_to_json_string(json_root));
+    /* printf("Parsed JSON: %s\n", json_object_to_json_string(json_root)); */
 
     /* exit */
     return 0;
@@ -191,20 +261,43 @@ int postRequest(){
   return 0;
 }
 
-int reportIncident(const char* filename) {
+int getThreatProfile(){
+
+    json_object *jrequest = json_object_new_object();    
+    //create threat request
+    json_object_object_add(jrequest, "incidentreportid", jobj);
+    json_root = jrequest;
+
+    if(postRequest(url_ThreatReport, "t") == 0){
+     //save scores
+     addProfile();
+    }
+
+    return 0;
+
+}
+
+int reportIncident(const char* filename){
 
   parseJson();
 
   if(json_root != NULL)
   {
      printf("send report.\n");
-     postRequest();
+
+     if(postRequest(url_IncidentReport,"r") == 0){
+      //Delete request once it was sent
+      //clearFile();
+      //Get threat profile
+      if (json_object_object_get_ex(jobj, "id", &jobj)) {   
+       
+          getThreatProfile();
+          
+          }
+     }
   }
 
  return 0;
-}
-
-int getThreatProfile(const char* filename) {
 }
 
 int main(int argc, char *argv[])
